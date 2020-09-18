4ceb6ec1f5e9b62fa8a3df1c8a45e46c667e0bdd
Arjun Bhatia <36422829+ArgonBoron23@users.noreply.github.com>
2020-09-18 17:07:01 -0400

    Formatting broke something, now fixed

--- Project 0 Files/P0_DistanceScenario.py
+++ Project 0 Files/P0_DistanceScenario.py
@@ -1,17 +1,19 @@
-from Common_Libraries.repeating_timer_lib import repeating_timer
-import os
-from Common_Libraries.p0_lib import *
-import time
 import sys
 sys.path.append('../')
+import time
+from Common_Libraries.p0_lib import *
 
-def update_sim():
+import os
+from Common_Libraries.repeating_timer_lib import repeating_timer
+
+def update_sim ():
     try:
         my_qbot.ping()
     except Exception as error_update_sim:
-        print(error_update_sim)
+        print (error_update_sim)
 
-speed = 0.1  # in m/s
+
+speed = 0.1 # in m/s
 my_qbot = qbot(speed)
 update_thread = repeating_timer(2, update_sim)
 
